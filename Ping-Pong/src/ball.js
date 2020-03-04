c128624c1573ca12d53c13024072e988b7b3fc15
Nicholas <nicho.tjornelund@gmail.com>
2020-03-04 19:16:49 +0100

    factor out crash code

--- Ping-Pong/src/ball.js
+++ Ping-Pong/src/ball.js
@@ -32,50 +32,11 @@ function ball(myGameArea, radius, color, x, y) {
             this.dy = 0;
             this.dx = 0;
             this.crashAble[0].y = myGameArea.canvas.height - 2;
+            this.score /= 2;
         }
 
-        /*
-        for (let i = 0; i < gameObjects.length; i++) {
-            if (gameObjects[i].type === 1) continue;
-            let otherobj = gameObjects[i];
-            var otherleft = otherobj.x;
-            var otherright = otherobj.x + (otherobj.width);
-            var othertop = otherobj.y;
-            var otherbottom = otherobj.y + (otherobj.height);
-            if((this.y > othertop-this.r) && (this.y < otherbottom+ this.r)&&(this.x > otherleft-this.r) && (this.x < otherright+this.r)) this.dy = -this.dy;
-        }
-
-         */
         for (let i = 0; i < this.crashAble.length; i++) {
-            let otherobj = this.crashAble[i];
-            var otherleft = otherobj.x;
-            var otherright = otherobj.x + (otherobj.width);
-            var othertop = otherobj.y;
-            var otherbottom = otherobj.y + (otherobj.height);
-
-            let bottom = this.y - this.r < otherbottom;
-            let top = this.y + this.r > othertop;
-
-            let left = this.x > otherleft;
-            let right = this.x < otherright;
-
-
-            //console.log(below+" "+over+" "+left+" "+right);
-
-            if (left && right && bottom && top) {
-                if (otherobj.type == 0) {
-                    let half = otherobj.width / 2;
-                    let a = this.x - (otherobj.x + half);
-                    let b = a / half;
-                    let angle = (-b * 2 * Math.PI / 5) + (Math.PI / 2);
-                    this.dx = this.v * Math.cos(angle);
-                    this.dy = -this.v * Math.sin(angle);
-                    this.score += 1;
-                } else {
-                    this.dx = -this.dx;
-                    this.dy = -this.dy;
-                }
-            }
+            this.crashAble[i].crash(this);
         }
         this.x += this.dx;
         this.y += this.dy;
--- Ping-Pong/src/plate.js
+++ Ping-Pong/src/plate.js
@@ -1,4 +1,4 @@
-function plate(myGameArea,width, height, color, x, y) {
+function plate(myGameArea, width, height, color, x, y) {
     this.type = 0;
     this.width = width;
     this.height = height;
@@ -12,17 +12,40 @@ function plate(myGameArea,width, height, color, x, y) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
     this.update = function () {
-        if(this.left){
+        if (this.left) {
             this.x -= 20;
             this.left = false;
         }
 
-        if (this.right){
+        if (this.right) {
             this.x += 20;
             this.right = false;
         }
 
-        if(this.x < 0) this.x = 0;
-        if(this.x > myGameArea.canvas.width) this.x = myGameArea.canvas.width-this.width;
+        if (this.x < 0) this.x = 0;
+        if (this.x > myGameArea.canvas.width) this.x = myGameArea.canvas.width - this.width;
+    }
+    this.crash = function (ball) {
+        var otherleft = this.x;
+        var otherright = this.x + (this.width);
+        var othertop = this.y;
+        var otherbottom = this.y + (this.height);
+
+        let bottom = ball.y - ball.r < otherbottom;
+        let top = ball.y + ball.r > othertop;
+
+        let left = ball.x > otherleft;
+        let right = ball.x < otherright;
+
+        if (left && right && bottom && top) {
+            let half = this.width / 2;
+            let a = ball.x - (this.x + half);
+            let b = a / half;
+            let angle = (-b * 2 * Math.PI / 5) + (Math.PI / 2);
+            ball.dx = ball.v * Math.cos(angle);
+            ball.dy = -ball.v * Math.sin(angle);
+            ball.score += 1;
+        }
+
     }
 }
