e32d138a3befe3d6e156c8e5dd54442aeb4885d6
Nicholas <nicho.tjornelund@gmail.com>
2020-03-17 13:25:46 +0100

    balancing stick done

--- Balancing-Stick/src/script.js
+++ Balancing-Stick/src/script.js
@@ -1,42 +1,44 @@
-var Example = Example || {};
-var sceneW = 1000;
+var sceneW = 1200;
 var sceneH = 700;
 
-Example.newtonsCradle = function () {
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Body = Matter.Body,
-        Composites = Matter.Composites,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        World = Matter.World;
+let population = 100;
+let config = {
+    model: [
+        {nodeCount: 3, type: "input"},
+        {nodeCount: 3, type: "hidden", activationfunc: activation.RELU},
+        {nodeCount: 2, type: "output", activationfunc: activation.SOFTMAX}
+    ],
+    mutationRate: 0.1,
+    crossoverMethod: crossover.RANDOM,
+    mutationMethod: mutate.RANDOM,
+    populationSize: population
+};
+// create engine
+var engine = Matter.Engine.create(),
+    world = engine.world;
 
-    // create engine
-    var engine = Engine.create(),
-        world = engine.world;
+var top = Matter.Bodies.rectangle(sceneW / 2, 0, sceneW * 2, 10, {
+    isStatic: true,
+    render: {fillStyle: "#000"}
+});
+var bottom = Matter.Bodies.rectangle(sceneW / 2, sceneH, sceneW * 2, 10, {
+    isStatic: true, render: {fillStyle: "#000"}
+});
+/*var left = Matter.Bodies.rectangle(0, sceneH / 2, 10, sceneH, {
+    isStatic: true, render: {fillStyle: "#000"}
+});
+var right = Matter.Bodies.rectangle(sceneW, sceneH / 2, 10, sceneH, {
+    isStatic: true, render: {fillStyle: "#000"}
+});*/
+Matter.World.add(world, [top, bottom]);
 
-    // create renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: sceneW,
-            height: sceneH,
-            showVelocity: true,
-            wireframes: false,
-            background: "#eee"
-        }
-    });
+var group = Matter.Body.nextGroup(true);
 
-    Render.run(render);
 
-    // create runner
-    var runner = Runner.create();
-    Runner.run(runner, engine);
-
-    var balancer = function () {
-        var container = Matter.Composite.create();
+var player = function (x, y) {
+    this.score = 0;
+    this.alive = true;
+    this.content = (function () {
         var vertices = [
             {x: 0, y: 0},
             {x: 110, y: 0},
@@ -48,17 +50,16 @@ Example.newtonsCradle = function () {
             {x: 0, y: 10},
             {x: 0, y: 0}
         ];
-        var group = Body.nextGroup(true);
 
-        var t = Matter.Bodies.fromVertices(400, 200, vertices, {
+        var t = Matter.Bodies.fromVertices(x, y, vertices, {
             collisionFilter: {
                 group: group
             }
         });
-        var cart = Matter.Bodies.rectangle(400, 280, 80, 20, {
+        var cart = Matter.Bodies.rectangle(x, y + 100, 80, 20, {
             collisionFilter: {
                 group: group
-            }
+            }, density: 0.005
         });
 
         var connector = Matter.Constraint.create({
@@ -68,91 +69,165 @@ Example.newtonsCradle = function () {
             stiffness: 0,
             length: 0
         });
-        Matter.Composite.addBody(container, t);
-        Matter.Composite.addBody(container, cart);
-        Matter.Composite.addConstraint(container, connector);
-        return container;
-    };
 
-
-    let b = new balancer();
-    World.add(world, b);
-
-    World.add(world, [
-        // walls
-        Matter.Bodies.rectangle(sceneW / 2, 0, sceneW, 10, {isStatic: true, render: {fillStyle: "#000"}}),
-        Matter.Bodies.rectangle(sceneW / 2, sceneH, sceneW, 10, {isStatic: true, render: {fillStyle: "#000"}}),
-        Matter.Bodies.rectangle(0, sceneH / 2, 10, sceneH, {isStatic: true, render: {fillStyle: "#000"}}),
-        Matter.Bodies.rectangle(sceneW, sceneH / 2, 10, sceneH, {isStatic: true, render: {fillStyle: "#000"}})
-    ]);
-
-
-    var left = false;
-    var right = false;
-    Matter.Events.on(engine, 'beforeUpdate', e=> {
-        if (left) {
-            Matter.Composite.translate(b, {x: -18, y: 0});
-        }
-        if (right) {
-            Matter.Composite.translate(b, {x: 18, y: 0});
-        }
-
-    });
-
-    window.addEventListener("keydown", e=> {
-        if (e.keyCode === 37) {
-            left = true;
-            right=false;
-        }
-        if (e.keyCode === 39){
-            right = true;
-            left = false;
-        }
-    });
-    window.addEventListener("keyup", e=> {
-        if(left)left=false;
-        if(right)right=false;
-    });
-
-
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    World.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    /*
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: 1000, y: 700 }
-    });
-*/
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        render: render,
-        canvas: render.canvas,
-        stop: function () {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
-    };
+        Matter.World.addBody(world, t);
+        Matter.World.addBody(world, cart);
+        Matter.World.addConstraint(world, connector);
+        return [t, cart,connector];
+    })();
 };
 
-if (typeof module !== 'undefined') {
-    module.exports = Example[Object.keys(Example)[0]];
-}
-Example.newtonsCradle();
+var players = [];
+
+var spawnPlayers = function () {
+    players = [];
+    for (let i = 0; i < population; i++) {
+        players.push(new player(500, 500));
+    }
+};
+
+var neat = new NEAT(config);
+spawnPlayers();
+/*setInterval(function () {
+    var alive = 0;
+    var bestScore = 0;
+    for (let i = 0; i < population; i++) {
+        if (balls[i].alive) {
+            alive++;
+            bestScore = Math.max(bestScore, balls[i].score);
+        }
+    }
+    console.log("alive: " + alive + " - bestScore: " + bestScore);
+}, 2000);*/
+
+
+// create renderer
+var render = Matter.Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: sceneW,
+        height: sceneH,
+        showVelocity: true,
+        wireframes: false,
+        background: "#eee"
+    }
+});
+
+Matter.Render.run(render);
+
+// create runner
+var runner = Matter.Runner.create();
+Matter.Runner.run(runner, engine);
+
+var count = 0;
+Matter.Events.on(engine, 'beforeUpdate', e => {
+    count++;
+
+    //finish check
+    if (count % 500) {
+        if (players.every(x => !x.alive)) {
+            console.log("finish!");
+            best = 0;
+            for (let i = 0; i < population; i++) {
+                best = Math.max(players[i].score,best);
+                neat.setFitness(players[i].score, i);
+                Matter.World.remove(world, players[i].content);
+            }
+            console.log("best score = "+best);
+            console.log(neat.export());
+            spawnPlayers();
+            neat.doGen();
+        }
+    }
+
+
+    //MISC
+    for (let i = 0; i < population; i++) {
+        //NN INPUT
+        let inn = [players[i].content[0].angle * 100, players[i].content[0].position.x, players[i].content[1].position.x];
+        neat.setInputs(inn, i);
+
+        //UPDATE FITNESS
+        if (players[i].alive) players[i].score += 1;
+
+        //DEAD CRITERION
+        if (Math.abs(players[i].content[0].angle) > 1.2) {
+            players[i].alive = false;
+        }
+
+        if (players[i].content[1].position.x < 50) {
+            players[i].alive = false;
+        }
+        if (players[i].content[1].position.x > 1150) {
+            players[i].alive = false;
+        }
+    }
+
+    //eval NN
+    neat.feedForward();
+
+    //NN RESULT -> DESCISION
+    let desicions = neat.getDesicions();
+    for (let i = 0; i < population; i++) {
+        if (desicions[i] === 0 && players[i].alive) {
+            Matter.Body.translate(players[i].content[1], {x: -18, y: 0});
+        } else {
+            Matter.Body.translate(players[i].content[1], {x: 18, y: 0});
+        }
+    }
+});
+
+/*var left = false;
+var right = false;
+var count = 0;
+Matter.Events.on(engine, 'beforeUpdate', e => {
+    if (left) {
+        Matter.Body.translate(players[0].content[1], {x: -18, y: 0});
+    }
+    if (right) {
+        Matter.Body.translate(players[0].content[1], {x: 18, y: 0});
+    }
+});
+
+window.addEventListener("keydown", e => {
+    if (e.keyCode === 37) {
+        left = true;
+        right = false;
+    }
+    if (e.keyCode === 39) {
+        right = true;
+        left = false;
+    }
+});
+window.addEventListener("keyup", e => {
+    if (left) left = false;
+    if (right) right = false;
+});*/
+
+
+// add mouse control
+var mouse = Matter.Mouse.create(render.canvas),
+    mouseConstraint = Matter.MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+Matter.World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+/*
+// fit the render viewport to the scene
+Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: 1000, y: 700 }
+});
+*/
+// context for MatterTools.Demo
